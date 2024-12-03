48ecc981f5f4ba966829f631b3b1e3c909214ece
VictorKreutzmann <kreutzmann@ruc.dk>
2024-12-03 14:13:47 +0100

    Enhance Navbar with dropdown menu and search functionality; update styles for improved layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,43 @@
-import React from 'react'
-import logo from '../assets/OMGDB_logo.png'
-import '../styles/Navbar.css'
+import React, { useState } from 'react';
+import logo from '../assets/OMGDB_logo.png';
+import '../styles/Navbar.css';
 
 function Navbar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   return (
     <div className='navbar'>
       <div className='leftSide'>
-        <img src={logo} alt='OMGDB Logo' />
+        <a href="/"><img src={logo} alt='OMGDB Logo' /></a>
+        <div 
+          className='dropdown' 
+          onMouseEnter={toggleDropdown} 
+          onMouseLeave={toggleDropdown}
+        >
+          <button className='dropdownButton'>Menu</button>
+          {dropdownOpen && (
+            <div className='dropdownContent'>
+              <a href="/Title">Movies</a>
+              <a href="/Title">Series</a>
+              <a href="/Persons">Actors</a>
+            </div>
+          )}
+        </div>
       </div>
-      <div className='rightSide'>dhdh</div>
-    </div>
-  )
+      <div className='center'>
+        <input className='searchbar' type='text' placeholder='Search OMGDB' />
+      </div>
+      <div className='rightSide'>
+        <div className='watchlist'>Watchlist</div>
+        <div className='signin'>Sign In</div>
+        </div>
+      </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
