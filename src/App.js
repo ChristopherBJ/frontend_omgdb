b96393c07e5105e8c414b99eb61a61425370d8d6
Christopher Bech-Jensen <bechjensen@ruc.dk>
2024-12-03 12:10:08 +0100

    Implement routing and layout structure: add Navbar, Footer, and Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
-
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <div>aaa</div>
-    </div>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
   );
 }
 
