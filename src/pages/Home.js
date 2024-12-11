16d8998a94c0f856a279a38ed3cdeafee17fc358
VictorKreutzmann <kreutzmann@ruc.dk>
2024-12-11 10:03:32 +0100

    Add Spinner component and update h2 styling in Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel, Container, Row, Col } from 'react-bootstrap';
+import { Carousel, Container, Row, Col, Spinner } from 'react-bootstrap';
 import '../styles/Home.css'; // Import the custom CSS
 
 const Home = () => {
